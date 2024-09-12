aea6691b7ef833fdbb9eb58a90d5f4589f6a232a
chittarisindhura <csindhura1279@gmail.com>
2024-09-12 10:33:40 +0500

    add

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "https://yehweh.vercel.app/",
+    origin: true,
     credentials: true,
     methods: "GET,PUT,POST,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
